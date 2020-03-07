47deda3bf634fd7765c5f4f5723005f53b9141ec
iamsantoshpro <santosh.bm302@gmail.com>
2020-03-07 12:08:14 +0530

    initial commit

---
+++ config.js
@@ -0,0 +1,4 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/81ab8b2b-be64-4888-a11a-c6b852719fcd/token";
+const instanceLocator = "v1:us1:81ab8b2b-be64-4888-a11a-c6b852719fcd";
+
+export { tokenUrl, instanceLocator }
