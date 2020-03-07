9a4cff03503c79e3db53fc70e105d6b390952f6f
iamsantoshpro <santosh.bm302@gmail.com>
2020-03-07 12:25:26 +0530

    Updates

---
+++ precache-manifest.01ae4046da63801b50364146373631c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0786529b5f17c3ce324f81798c1b231f",
+    "url": "/chatapp/index.html"
+  },
+  {
+    "revision": "75875919c5f592806360",
+    "url": "/chatapp/static/js/2.8170849b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/chatapp/static/js/2.8170849b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd04041f10ef664fdf26",
+    "url": "/chatapp/static/js/main.090e7d58.chunk.js"
+  },
+  {
+    "revision": "c5109094fa08c2172f4e",
+    "url": "/chatapp/static/js/runtime-main.8e94787f.js"
+  }
+]);
